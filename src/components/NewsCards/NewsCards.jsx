25b3a32ea6a605a20f1ce7eaa3a4f5934b9edd08
lucifer2355 <dgajjar999@gmail.com>
2020-08-12 15:17:18 +0530

    add news card style

--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -17,7 +17,7 @@ const NewsCards = ({ articles }) => {
       >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
-            <NewsCard article={article} i={i} key={i} />
+            <NewsCard articles={article} i={i} key={i} />
           </Grid>
         ))}
       </Grid>
