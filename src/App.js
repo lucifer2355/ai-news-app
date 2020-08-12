00c9a048d1976b3165162973d94f5f9900b2cd58
lucifer2355 <dgajjar999@gmail.com>
2020-08-12 12:50:59 +0530

    add news card atticles component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 import "./App.css";
+import NewsCard from "./components/NewsCard/NewsCard";
 
 const alanKey =
   "56a7df4a97c62680c6267c06bfdeb48e2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
-          console.log(articles);
+          setNewsArticles(articles);
         }
       },
     });
@@ -21,6 +24,7 @@ function App() {
   return (
     <div className='app'>
       <h1>Alan AI News Application</h1>
+      <NewsCard articles={newsArticles} />
     </div>
   );
 }
