ec2ae7cc07f025beda2d85db03599648dba518c4
lucifer2355 <dgajjar999@gmail.com>
2020-08-12 12:08:38 +0530

    add alan key & voice button

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 
 import "./App.css";
 
+const alanKey =
+  "56a7df4a97c62680c6267c06bfdeb48e2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
 function App() {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("This code was executed");
+        }
+      },
+    });
+  }, []);
 
   return (
     <div className='app'>
