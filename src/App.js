b73dfdfc5b2ba158dc27ed605354f397778d2fdc
lucifer2355 <dgajjar999@gmail.com>
2020-08-12 17:56:05 +0530

    add active class style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 import "./App.css";
+import useStyles from "./style";
 import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey =
   "56a7df4a97c62680c6267c06bfdeb48e2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(0);
 
   useEffect(() => {
     alanBtn({
@@ -16,15 +19,23 @@ function App() {
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
         }
       },
     });
   }, []);
 
   return (
-    <div className='app'>
-      <h1>Alan AI News Application</h1>
-      <NewsCards articles={newsArticles} />
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src='https://alan.app/voice/images/previews/preview.jpg'
+          alt='alanLogo'
+          className={classes.alanLogo}
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -27,7 +27,7 @@ const infoCards = [
   },
 ];
 
-const NewsCards = ({ articles }) => {
+const NewsCards = ({ articles, activeArticle }) => {
   const classes = useStyles();
 
   if (!articles.length) {
@@ -80,7 +80,12 @@ const NewsCards = ({ articles }) => {
       >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
-            <NewsCard articles={article} i={i} key={i} />
+            <NewsCard
+              articles={article}
+              i={i}
+              key={i}
+              activeArticle={activeArticle}
+            />
           </Grid>
         ))}
       </Grid>
