8e36cccb3dd8709cf284becf46e37e88b4d2b26c
lucifer2355 <dgajjar999@gmail.com>
2020-08-12 18:49:43 +0530

    add information about developer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Typography } from "@material-ui/core";
+import wordsToNumbers from "words-to-numbers";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import wordsToNumber from "words-to-numbers";
 
-import "./App.css";
+import { NewsCards } from "./components";
 import useStyles from "./style";
-import NewsCards from "./components/NewsCards/NewsCards";
 
-const alanKey =
-  "56a7df4a97c62680c6267c06bfdeb48e2e956eca572e1d8b807a3e2338fdd0dc/stage";
-
-function App() {
-  const classes = useStyles();
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
   const [newsArticles, setNewsArticles] = useState([]);
-  const [activeArticle, setActiveArticle] = useState(-1);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
-      key: alanKey,
+      key:
+        "56a7df4a97c62680c6267c06bfdeb48e2e956eca572e1d8b807a3e2338fdd0dc/stage",
       onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
           setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
         } else if (command === "highlight") {
           setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
         } else if (command === "open") {
@@ -30,7 +32,14 @@ function App() {
               : number;
           const article = articles[parsedNumber - 1];
 
-          window.open(article.url, "_blank");
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
         }
       },
     });
@@ -39,15 +48,33 @@ function App() {
   return (
     <div>
       <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant='h5' component='h2'>
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant='h5' component='h2'>
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
         <img
           src='https://alan.app/voice/images/previews/preview.jpg'
-          alt='alanLogo'
           className={classes.alanLogo}
+          alt='logo'
         />
       </div>
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
-}
+};
 
 export default App;
