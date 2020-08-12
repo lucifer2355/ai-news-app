9f0cf713c6969844620e59ac14478487d0a257ce
lucifer2355 <dgajjar999@gmail.com>
2020-08-12 16:12:34 +0530

    display default data

--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -38,7 +38,34 @@ const NewsCards = ({ articles }) => {
           container
           alignItems='stretch'
           spacing={3}
-        ></Grid>
+        >
+          {infoCards.map((infoCard) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              className={classes.infoCard}
+            >
+              <div
+                className={classes.card}
+                style={{ backgroundColor: infoCard.color }}
+              >
+                <Typography variant='h5'>{infoCard.title}</Typography>
+                {infoCard.info ? (
+                  <Typography variant='h6'>
+                    <strong>{infoCard.title.split(" ")[2]}</strong>: <br />
+                    {infoCard.info}
+                  </Typography>
+                ) : null}
+                <Typography variant='h6' component='h6'>
+                  Try saying: <br /> <i>{infoCard.text}</i>
+                </Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
     );
   }
