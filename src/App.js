bba93ff0e829c759a16e630997911b12e1efd0de
lucifer2355 <dgajjar999@gmail.com>
2020-08-12 11:57:02 +0530

    add alan & material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
 import "./App.css";
 
 function App() {
+  useEffect(() => {}, []);
+
   return (
     <div className='app'>
-      <h1>Hello World</h1>
+      <h1>Alan AI News Application</h1>
     </div>
   );
 }
